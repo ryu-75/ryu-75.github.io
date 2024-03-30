be601bfd22efa6d6da3e0691d2f97cf872e4b6cc
Sasha <sasha.lorion@gmail.com>
2024-03-30 12:35:39 +0100

    [CREATE] - EducationComponent

---
+++ app/front/src/components/Education.vue
@@ -0,0 +1,31 @@
+<template>
+    <h1 class="flex justify-center">Formations</h1>
+    <div class="flex-col">
+        <div class="flex flex-row justify-between items-center">
+            <h3>École 42</h3>
+            <p>2022 - Actuel</p>    
+        </div>
+        <p class="m-6 text-lg">RNCP 6 - Développement d'application</p>
+        <ul class="m-6">
+            <li class="text-xl">Lorem ipsum</li>
+            <li class="text-xl">Lorem ipsum</li>
+            <li class="text-xl">Lorem ipsum</li>
+            <li class="text-xl">Lorem ipsum</li>
+        </ul>
+    </div>
+    <hr class="h-px my-6 bg-gray-200 border-0 dark:bg-gray-700">
+    <div class="flex-col">
+        <div class="flex flex-row justify-between items-center">
+            <h3>ISART Digital</h3>
+            <p>2015 - 2017</p>    
+        </div>
+        <p class="m-6 text-lg">Music & Sound Design</p>
+        <ul class="m-6">
+            <li class="text-xl">Lorem ipsum</li>
+            <li class="text-xl">Lorem ipsum</li>
+            <li class="text-xl">Lorem ipsum</li>
+            <li class="text-xl">Lorem ipsum</li>
+        </ul>
+    </div>
+    <hr class="h-px my-6 bg-gray-200 border-0 dark:bg-gray-700">
+</template>
