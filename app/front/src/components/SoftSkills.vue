915fe07b00b87a57aeaf53f99a0212738e314d69
Sasha <sasha.lorion@gmail.com>
2024-03-30 12:37:28 +0100

    [CREATE] - SoftSkillsComponent

---
+++ app/front/src/components/SoftSkills.vue
@@ -0,0 +1,31 @@
+
+<template>
+    <h1 class="flex justify-center">Compétences interpersonnel</h1>
+    <div class="grid grid-rows-2 grid-flow-col gap-2">
+        <div class="text-center m-12">
+            <p class="text-4xl text-shadow-lg shadow-gray-400">Travail en équipe</p>
+        </div>
+        <div class="text-center m-12">
+            <p class="text-4xl text-shadow-lg shadow-gray-400">Rigoureuse</p>
+        </div>
+        <div class="text-center m-12">
+            <p class="text-4xl text-shadow-lg shadow-gray-400">Autonome</p>
+        </div>
+        <div class="text-center m-12">
+            <p class="text-4xl text-shadow-lg shadow-gray-400">Curieuse</p>
+        </div>
+        <div class="text-center m-12">
+            <p class="text-4xl text-shadow-lg shadow-gray-400">Méthodique</p>
+        </div>
+        <div class="text-center m-12">
+            <p class="text-4xl text-shadow-lg shadow-gray-400">Créative</p>
+        </div>
+        <div class="text-center m-12">
+            <p class="text-4xl text-shadow-lg shadow-gray-400">Pensée critique</p>
+        </div>
+        <div class="text-center m-12">
+            <p class="text-4xl text-shadow-lg shadow-gray-400">Empathie</p>
+        </div>
+    </div>
+    <hr class="h-px my-8 bg-gray-200 border-0 dark:bg-gray-700">
+</template>
