674f4fad1d9d72803ff067a0e6386832c06fc2ab
Sasha <sasha.lorion@gmail.com>
2024-04-01 13:39:22 +0200

    [UPDATE] - Nesting

--- app/front/src/components/SoftSkills.vue
+++ app/front/src/components/SoftSkills.vue
@@ -1,6 +1,6 @@
 
 <template>
-    <h1 class="flex justify-center">Compétences interpersonnel</h1>
+    <h1 class="flex justify-center">Compétences interpersonnelles</h1>
     <div class="grid grid-rows-2 grid-flow-col gap-2">
         <div class="text-center m-12">
             <p class="text-4xl text-shadow-lg shadow-gray-400">Travail en équipe</p>
@@ -27,5 +27,5 @@
             <p class="text-4xl text-shadow-lg shadow-gray-400">Empathie</p>
         </div>
     </div>
-    <hr class="h-px my-8 bg-gray-200 border-0 dark:bg-gray-700">
+    <hr class="h-px my-8 bg-gray-200 border-0 dark:bg-gray-700 m-32">
 </template>
