fe62c612db92991ff92362a68e8cc8d4ec20c023
Sasha <sasha.lorion@gmail.com>
2024-04-01 13:39:53 +0200

    [UPDATE] - Nesting

--- app/front/src/components/Education.vue
+++ app/front/src/components/Education.vue
@@ -1,31 +1,32 @@
 <template>
     <h1 class="flex justify-center">Formations</h1>
-    <div class="flex-col">
-        <div class="flex flex-row justify-between items-center">
-            <h3>École 42</h3>
-            <p>2022 - Actuel</p>    
+    <div class="mx-44">
+        <div class="flex-col">
+            <div class="flex flex-row justify-between items-center">
+                <h3>École 42</h3>
+                <p>2022 - Actuel</p>    
+            </div>
+            <p class="m-6 text-lg">RNCP 6 - Développement d'application</p>
+            <ul class="m-6">
+                <li class="text-xl">Lorem ipsum</li>
+                <li class="text-xl">Lorem ipsum</li>
+                <li class="text-xl">Lorem ipsum</li>
+                <li class="text-xl">Lorem ipsum</li>
+            </ul>
         </div>
-        <p class="m-6 text-lg">RNCP 6 - Développement d'application</p>
-        <ul class="m-6">
-            <li class="text-xl">Lorem ipsum</li>
-            <li class="text-xl">Lorem ipsum</li>
-            <li class="text-xl">Lorem ipsum</li>
-            <li class="text-xl">Lorem ipsum</li>
-        </ul>
-    </div>
-    <hr class="h-px my-6 bg-gray-200 border-0 dark:bg-gray-700">
-    <div class="flex-col">
-        <div class="flex flex-row justify-between items-center">
-            <h3>ISART Digital</h3>
-            <p>2015 - 2017</p>    
+        <div class="flex-col">
+            <div class="flex flex-row justify-between items-center">
+                <h3>ISART Digital</h3>
+                <p>2015 - 2017</p>    
+            </div>
+            <p class="m-6 text-lg">Music & Sound Design</p>
+            <ul class="m-6">
+                <li class="text-xl">Lorem ipsum</li>
+                <li class="text-xl">Lorem ipsum</li>
+                <li class="text-xl">Lorem ipsum</li>
+                <li class="text-xl">Lorem ipsum</li>
+            </ul>
         </div>
-        <p class="m-6 text-lg">Music & Sound Design</p>
-        <ul class="m-6">
-            <li class="text-xl">Lorem ipsum</li>
-            <li class="text-xl">Lorem ipsum</li>
-            <li class="text-xl">Lorem ipsum</li>
-            <li class="text-xl">Lorem ipsum</li>
-        </ul>
     </div>
-    <hr class="h-px my-6 bg-gray-200 border-0 dark:bg-gray-700">
+    <hr class="h-px my-8 bg-gray-200 border-0 dark:bg-gray-700 m-32">
 </template>
