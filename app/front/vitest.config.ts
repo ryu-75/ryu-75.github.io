52c57ba0cf9d77f044999366bb7576a8f6b2cee0
Sasha <sasha.lorion@gmail.com>
2024-04-19 15:33:07 +0200

    [UPDATE] - .json settings

--- app/front/vitest.config.ts
+++ app/front/vitest.config.ts
@@ -5,6 +5,7 @@ import viteConfig from './vite.config'
 export default mergeConfig(
   viteConfig,
   defineConfig({
+    base:'/subscriber:',
     test: {
       environment: 'jsdom',
       exclude: [...configDefaults.exclude, 'e2e/*'],
