4f5447913895bbc1acdb52777e71a1ed724acdc7
Sasha <sasha.lorion@gmail.com>
2024-04-23 16:28:12 +0200

    [FIXED] - Routing

--- app/front/src/router/index.ts
+++ app/front/src/router/index.ts
@@ -19,7 +19,7 @@ const router = createRouter({
       component: () => import('@/components/Contact.vue')
     },
     {
-      path: '/:pathMatch(.*)*',
+      path: '/:catchAll(.*)',
       redirect: '/'
     }
   ]
