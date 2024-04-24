405e010cdf8c0aabee7eb9ab025f77f23a395a5e
Sasha <sasha.lorion@gmail.com>
2024-04-24 14:28:20 +0200

    [CONF] - .json

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/deploying-vite-project-example/',
+  base: '/welcome/',
   plugins: [
     vue(),
     vueJsx(),
